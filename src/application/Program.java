2cbe3259c43171e93cea2e11d7af431fd735a7f7
Caio Barros <caiobarros1712@gmail.com>
2024-01-05 14:52:27 -0300

    SpecialMove Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,13 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/Q/R): ");
+					String type = sc.nextLine();
+					chessMatch.replacePrometedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
