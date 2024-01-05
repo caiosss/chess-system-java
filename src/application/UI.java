12e3ddc8d02b9aa14a39b58cc0a786c4d0c18ace
Caio Barros <caiobarros1712@gmail.com>
2024-01-05 12:09:01 -0300

    Check Logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,6 +58,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -107,7 +110,6 @@ public class UI {
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
-		System.out.println("Captured pieces: ");
 		System.out.print("Black: ");
 		System.out.print(ANSI_PURPLE);
 		System.out.println(Arrays.toString(black.toArray()));
